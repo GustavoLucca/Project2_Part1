7dd5b98755a4d15e792c996541d71786d51be46b
GustavoLucca <gustavo.lucca@slu.edu>
2024-09-30 22:22:45 -0500

    Created shift_left and wrote pseudocode for key_gen.

--- key_scheduler.cpp
+++ key_scheduler.cpp
@@ -5,15 +5,24 @@
 #include <bitset>
 using namespace std;
 
-string shift_left(string sub_key){
-    // write code from here
+string shift_left(string sub_key, int shiftNum){
+    for (int i = 0; i < shiftNum; ++i){ //Shifts through every bit, doing shiftNum amount of loops.
+        char firstBit = sub_key[0]; //Register what the first bit is.
+        sub_key = sub_key.substr(1) + firstBit; // Move every bit left one spot and add the first bit at the end.
+    }
     
     return sub_key;
 } 
 
 vector<string> key_gen(string key){
     vector<string> sub_keys;
-    //write code from here
+    //Import 64-bit key
+    // permChoice1 = 56 bits of key
+    //C = 28 bits & D = 28 bits (half & half)
+    // for (int i = 0; i < 16; i++){
+        // Left shift C and left shift D
+        // permChoice2 = C+D in 48 bits
+        // add permChoice2 to sub_keys
     
     return sub_keys;
 }
