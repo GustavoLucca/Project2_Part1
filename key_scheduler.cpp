ceba10991c6cf39baaa70603b302f87e0966b06e
GustavoLucca <gustavo.lucca@slu.edu>
2024-10-01 20:54:41 -0500

    Created key_gen.

--- key_scheduler.cpp
+++ key_scheduler.cpp
@@ -16,13 +16,33 @@ string shift_left(string sub_key, int shiftNum){
 
 vector<string> key_gen(string key){
     vector<string> sub_keys;
-    //Import 64-bit key
-    // permChoice1 = 56 bits of key
-    //C = 28 bits & D = 28 bits (half & half)
-    // for (int i = 0; i < 16; i++){
-        // Left shift C and left shift D
-        // permChoice2 = C+D in 48 bits
-        // add permChoice2 to sub_keys
-    
+    //First, we find the mappings for the bits that will be processed in Permuted Choice 1 and then successively in Permuted Choice 2.
+    vector<int> PCone = {57,49,41,33,25,17,9,1,58,50,42,34,26,18,10,2,59,51,43,35,27,19,11,3,60,52,44,36,63,55,47,39,31,23,15,7,62,54,46,38,30,22,14,6,61,53,45,37,29,21,13,5,28,20,12,4};
+    vector<int> PCtwo = {14,17,11,24,1,5,3,28,15,6,21,10,23,19,12,4,26,8,16,7,27,20,13,2,41,52,31,37,47,55,30,40,51,45,33,48,44,49,39,56,34,53,46,42,50,36,29,32};
+    //Then, we apply PCone to the entire key.
+    string permChoice1;
+    for (int i = 0; i < 56; i++) {
+        permChoice1 += key[PCone[i] - 1];
+    }
+    //Then, we split the new 56 bit key into two.
+    string C = permChoice1.substr(0, 28);  // First 28 bits
+    string D = permChoice1.substr(28, 28); // Next 28 bits
+    //Now, we generate the subkeys and apply PCtwo to all of them.
+    for (int i = 0; i < 16; i++) {
+        if (i == 0 || i == 1 || i == 8 || i == 15) {
+            C = shift_left(C, 1);
+            D = shift_left(D, 1);
+        } else {
+            C = shift_left(C, 2);
+            D = shift_left(D, 2);
+        }
+        string CD = C + D;
+        //Now that the separate keys have been shifted according to the schedule and joined back together, we apply Permuted Choice 2 to it and add it to the sub_keys vector.
+        string permChoice2;
+        for (int j = 0; j < 48; j++) {
+            permChoice2 += CD[PCtwo[j] - 1];
+        }
+        sub_keys.push_back(permChoice2);
+    }
     return sub_keys;
 }
